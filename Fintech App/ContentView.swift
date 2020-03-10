2c8b9de1135120b1a5737d5d4f803b9cb1b53491
Ogbonda Chiziaruhoma <ogbondachiziaruhoma@gmail.com>
2020-03-10 23:16:36 +0100

    Initial Commit

---
+++ Fintech App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fintech App
+//
+//  Created by Chiziaruhoma Ogbonda on 10/03/2020.
+//  Copyright © 2020 Chiziaruhoma Ogbonda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fintech AppTests/Fintech_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fintech_AppTests.swift
+//  Fintech AppTests
+//
+//  Created by Chiziaruhoma Ogbonda on 10/03/2020.
+//  Copyright © 2020 Chiziaruhoma Ogbonda. All rights reserved.
+//
+
+import XCTest
+@testable import Fintech_App
+
+class Fintech_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
